895f4d22b15e046bc031129504c8589131d631d2
Jose Gomez Parra <josegomezparra@Joses-MacBook-Pro.local>
2024-12-27 16:19:22 -0600

    update

--- odoo/addons/pos_hide_out_of_stock/static/src/js/product_filter.js
+++ odoo/addons/pos_hide_out_of_stock/static/src/js/product_filter.js
@@ -4,21 +4,24 @@ odoo.define('pos_hide_out_of_stock.ProductFilter', function(require) {
     const models = require('point_of_sale.models');
     const PosDB = require('point_of_sale.DB');
 
-    // Extender modelo de producto
+    // Log para confirmar la carga del archivo
+    console.log('Cargando módulo POS Hide Out of Stock Products');
+
     models.load_models([{
         model: 'product.product',
         fields: ['name', 'qty_available', 'type', 'available_in_pos'],
         domain: [['available_in_pos', '=', true], ['type', '=', 'product']],
         loaded: function(self, products) {
-            // Filtrar productos con cantidad disponible mayor a 0
+            console.log('Productos cargados:', products.length);
             const filteredProducts = products.filter(product => product.qty_available > 0);
+            console.log('Productos después del filtro:', filteredProducts.length);
             self.db.add_products(filteredProducts);
         },
     }], { 'after': 'product.product' });
 
-    // Sobrescribir la función de la base de datos para devolver solo productos con stock
     PosDB.include({
         add_products: function(products) {
+            console.log('Añadiendo productos a la base de datos del POS');
             const filteredProducts = products.filter(product => product.qty_available > 0);
             this._super(filteredProducts);
         },
